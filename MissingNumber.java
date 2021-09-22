bc1bea4d6cad3cc88f4b764e1fd26625db31f3ec
Kairamkondapranathi <89980698+Kairamkondapranathi@users.noreply.github.com>
2021-09-22 17:07:43 +0530

    MissingNumber

--- MissingNumber.java
+++ MissingNumber.java
@@ -4,10 +4,18 @@ public class MissingNumber {
 	public static void main(String [] args) {
 		int a[]= {1,2,3,4,5,6,7,8,9,11,12,13,14,15};
 		for(int i=0;i<a.length;i++) {
-				if(a[i]!=a[i+1]-1) {
-					System.out.println("The missing number is "+(a[i]+1));
-				}
+			sum1=sum1+a[i];
+		}
+		
+			int sum2=0;
+			for(int i=0;i<=15;i++) {
+				sum2=sum2+i;
 			}
+			
+			
+			System.out.println("The missing number is " +(sum2-sum1));
+				
+			
 		}
 	}
 
